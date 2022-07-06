ea25c9ad33ae430db1de0e14410fad8dd40f3ef9
Maria goretti <mgmkimani@gmail.com>
2022-07-06 16:51:54 +0000

    add

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-Module 3-write_file#!/usr/bin/python3
+""" Module 1-write_file
+Contains function that creates file and returns num chars written
 """
 
-Contains function that writes to text file and returns num chars written
-"""
+
 def write_file(filename="", text=""):
-    """writes to text file and returns num chars written"""
+    """ writes to text file and returns the number of characters written """
     with open(filename, mode="w", encoding="utf-8") as f:
         return(f.write(text))
