8b431041aa30155c2d7f0c88d81fcf444f1f2098
Mariagoretti05 <mgmkimani@gmail.com>
2022-09-22 19:08:47 +0000

    python

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request with a header variable "X-School-User-Id", with the value 98
-curl -sH "X-School-User-Id: 98" "$1"
+# GET request with a header variable "X-School-User-Id", with the value 98
+curl -sLH "X-School-User-Id: 98" "$1"
